50a054198b2863a419066b35f9b9cb1ee3bbb402
Ji hyeon Lee <jihen0629@naver.com>
2019-01-23 03:09:59 +0000

    D16 | 190123AM | CRUD & ORM

---
+++ flask/orm/app.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# flask
+app = Flask(__name__)
+
+# sqlalchemy 설정
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db_flask.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# sqlalchemy 초기화
+db = SQLAlchemy(app)
+
+# migrate 초기화
+migrate = Migrate(app, db)
+
+
+# table 만들기
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique = True, nullable=False)
+    memo = db.Column(db.Text)
+    
+    def __repr__(self):
+        return f'<User {self.id}: {self.username}, {self.email}>'
+
+# 명령어 
+# falsk db
+
+# 정리
+# [Create]
+# INSERT INTO users (username, email) VALUES ('Jihyeon','jihen0629@naver.com')
+# user =Ueser(username='Jihyeon', email='jihen0629@naver.com')
+# db.session.add(user)
+# db.session.commit()
+
+# [Read]
+# SELECT * FROM users;
+# user = User.query.all() # 복수
+
+# SELECT * FROM users WHERE username='Jihyeon';
+# user = User.query.filter_by(username='Jihyeon').all()
+
+# SELECT * FROM users WHERE username='Jihyeon' LIMIT 1;
+# user = User.query.filter_by(username='Jihyeon').first()
+
+# user = User.query.get(2)
+# primary key만 get으로 가져올 수 있음.
+
+# users = User.query.filter(User.email.like("%hen%")).all()
