35910484c850b555020a7c97425a90ee5e83f545
Ji hyeon Lee <jihen0629@naver.com>
2019-01-29 08:34:49 +0000

    D20 | 190129 PM| CRUD

--- django/17workshop/migrate/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Migrate
-# Register your models here.
-class MigrateAdmin(admin.ModelAdmin):
-    list_display=('name','email','birthday','age',)
-    
-admin.site.register(Migrate, MigrateAdmin)
-
