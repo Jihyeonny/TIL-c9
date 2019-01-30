c9644b2a2358682ea39cb8d009eab9ec84962701
Ji hyeon Lee <jihen0629@naver.com>
2019-01-30 03:23:17 +0000

    D21 |190130 AM | Django url names

--- django/19workshop/workshop/students/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    name = models.CharField(max_length =40)
-    email = models.CharField(max_length =100)
-    birthday = models.DateField()
-    age = models.IntegerField()
