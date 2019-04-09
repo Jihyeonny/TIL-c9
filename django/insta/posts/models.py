602322ee16321eda921fb1502ee7bfca17b7d845
Ji hyeon Lee <jihen0629@naver.com>
2019-04-09 08:44:36 +0000

    0409

--- django/insta/posts/models.py
+++ django/insta/posts/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
+
+def post_image_path(instance,filename):
+    # return 'posts/{}/{}'.format(instance.content,filename)
+    # return f'posts/{instance.content}/{filename}'
+    # return f'posts/{instance.content}/{instance.content}.jpg'
+    return f 'posts/images/{filename}'
 # Create your models here.
 
 class Post(models.Model):
     content=models.TextField()
+    # image=models.ImageField(blank=True)
+    image=ProcessedImageField(
+                    upload_to='posts/images', #저장위치
+                    processors=[ResizeToFill(600,600)], #처리할 작업 목록
+                    format='JPEG', #저장포멧
+                    options={'quality':90}, #옵션
+        )
